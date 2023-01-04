f19936fc549f13e791540871b823b4deda57c76d
Glenn Michael <gmdv@pm.me>
2023-01-05 05:40:02 +0800

    day 2 am

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,20 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    // base case 1
+    if (!curr) {
+        return false;
+    }
+    // base case 2
+    if (curr.value === needle) {
+        return true;
+    }
 
+    if (curr.value < needle) {
+        return search(curr.right, needle);
+    }
+
+    return search(curr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
